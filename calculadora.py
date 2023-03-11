157869a3cdbe90ae402fbcfe7f3bee1e3e84fc59
Paola-GJ <lp.garciaj@gmail.com>
2023-03-11 00:21:27 -0500

    cambios espiral

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,27 @@
 num1=5
 num2=-2
 
-def suma(numero1,numero2):
-    total=numero1+numero2
-    return total
+#def suma(numero1,numero2):
+#    total=numero1+numero2
+#    return total
 
-r=suma(num1,num2)
-print("el resultado es: ",r)
+#r=suma(num1,num2)
+#print("el resultado es: ",r)
+
+import turtle
+import colorsys
+
+t=turtle.Turtle()
+s=turtle.Screen()
+s.bgcolor("black")
+t.speed(0)
+n=36
+h=0
+for i in range(460):
+    c=colorsys.hls_to_rgb(h,1,0.9)
+    h+=1/n
+    t.color(c)
+    t.left(145)
+    for j in range(5):
+        t.forward(300)
+        t.left(150)
